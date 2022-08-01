0514203efbd19d26ed5b66c67c5edce718c2d9db
Samar Cherni <scherni@dom.tti>
2022-08-01 10:26:08 +0100

    update

--- Act.7-2/src/app/details/details.component.ts
+++ Act.7-2/src/app/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,Input } from '@angular/core';
 import { ArticlesService } from '../services/articles.service';
 import { Articles } from '../articles';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -10,13 +11,20 @@ import { Router } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   // @Input() public deatilsfromarticle;
-  constructor( private router:Router)
+  article: Articles | undefined;
+  constructor( private router:Router,  private route: ActivatedRoute,
+    private articleService: ArticlesService)
      { }
 
   ngOnInit(): void {
+    this.GoToDetails();
   }
-  GoToDetails(){
-    this.router.navigate(["/articles"])
+  
+
+  GoToDetails(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.articleService.getDetail(id)
+      .subscribe(article => this.article = article);
   }
 
 }
