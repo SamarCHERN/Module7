81aec63f773255daab16a78693ea05a641b3b508
Samar Cherni <scherni@dom.tti>
2022-08-01 10:05:53 +0100

    update

--- Act.7-2/src/app/services/articles.service.ts
+++ Act.7-2/src/app/services/articles.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Articles } from '../articles';
 import { ARTICLES } from '../list_articles';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +10,11 @@ export class ArticlesService {
   getArticles(): Articles[] {
     return ARTICLES;
   }
-  getDetail(){
-    
+  getDetail(id: number):Observable<Articles> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = ARTICLES.find(h => h.id === id)!;
+    return of(hero);
   }
   constructor() { }
 }
